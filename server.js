b33a34330841fbdbc39441b7b438d95b4a40c0aa
rachela1 <rachel_kaitlin01@hotmail.com>
2023-07-11 21:54:15 -0500

    fixed 404 errors missing /

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const PORT = process.env.PORT ?? 3001;
+const fs = require('fs');
 const app = express();
 //const db = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
-
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('api/notes', (req, res) => {
-    const newNote = fs.writeFileSync('./db/db.json', 'utf8')
+app.get('/api/notes', (req, res) => {
+    const newNote = fs.readFileSync('./db/db.json', 'utf8');
     const notes = JSON.parse(newNote);
     return res.json(notes);
 });
 
-app.post('api/notes', (req, res) => {
-    const newNote = fs.writeFileSync('./db/db.json', 'utf8')
+app.post('/api/notes', (req, res) => {
+    const newNote = fs.readFileSync('./db/db.json', 'utf8');
     const notes = JSON.parse(newNote);
-    notes.push({...req.body, id: uuidv4()});
+    notes.push({ ...req.body, id: uuidv4() });
     const data = JSON.stringify(notes);
-    fs.writeFileSync('./db/db.json', data)
+    fs.writeFileSync('./db/db.json', data);
     return res.json('success');
 });
 
