9a2bcf11c06cfc9603fc4c98b39a4521bfff1334
rachela1 <rachel_kaitlin01@hotmail.com>
2023-07-11 22:17:22 -0500

    added delete request for notes

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ app.post('/api/notes', (req, res) => {
     return res.json('success');
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const newNote = fs.readFileSync('./db/db.json', 'utf8');
+    const notes = JSON.parse(newNote);
+    const filteredNotes = notes.filter((note) => note.id !== req.params.id);
+    const data = JSON.stringify(filteredNotes);
+    fs.writeFileSync('./db/db.json', data);
+    return res.json('success');
+});
+
 app.get('*', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/index.html'));
 });
